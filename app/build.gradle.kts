fe9785376fd8b3b1e9dde6094ef0ebdbc9a8343b
zm123723 <1123950493@qq.com>
2026-04-15 14:36:04 +0800

    Add activity-ktx dependency to fix viewModels

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,8 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.activity:activity-ktx:1.8.2")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
